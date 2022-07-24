e6f72d418c64be4aba02113adc72d9b925e70e3b
chirag-ghosh <cghosh828049@gmail.com>
2022-07-24 21:29:41 +0530

    Fix syntax; Modify .gitignore;

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
-<script lang="ts">
+<script setup lang="ts">
   import axios from 'axios'
-  const images: string[] = [];
 
-  export default {
-    data() {
-      return images
-    },
-    mounted() {
-      axios.get("https://api.imgur.com/3/album/lKLDuiF", {
-        headers: {
-          Authorization: "Client-ID ab142a02ee43a2c"
-        }
-      })
-        .then((response) => {
-          images.push(...response.data.data.images)
-        })
-        .catch((err) => console.log(err));
-    }
+  type ImgurImage = {
+    id:            string;
+    title:         string;
+    description:   string;
+    datetime:      number;
+    type:          string;
+    animated:      boolean;
+    width:         number;
+    height:        number;
+    size:          number;
+    views:         number;
+    bandwidth:     number;
+    vote:          null;
+    favorite:      boolean;
+    nsfw:          null;
+    section:       null;
+    account_url:   null;
+    account_id:    null;
+    is_ad:         boolean;
+    in_most_viral: boolean;
+    has_sound:     boolean;
+    tags:          any[];
+    ad_type:       number;
+    ad_url:        string;
+    edited:        string;
+    in_gallery:    boolean;
+    link:          string;
   }
+
+  const images: ImgurImage[] = [];
+
+  function fetchImages() {
+    axios.get("https://api.imgur.com/3/album/lKLDuiF", {
+      headers: {
+        Authorization: "Client-ID ab142a02ee43a2c"
+      }
+    })
+      .then((response) => {
+        images.push(...response.data.data.images)
+      })
+      .catch((err) => console.log(err));
+  }
+
+  fetchImages()
 </script>
 
 <template>
@@ -26,7 +53,7 @@
   </div>
   <ul>
     <li v-for="image in images" :key="image.id">
-      <img src={{image.link}} alt={{image.title}} />
+      <img :src="image.link" :alt="image.title" />
     </li>
   </ul>
 </template>
